dabfdf95e76a17bbab13d9f5933e5aef32caac62
Shirin <shirinetezazian@gmail.com>
2021-12-12 11:29:18 -0700

    Implement BFSelect in O(n) time and space complexity

--- index.js
+++ index.js
@@ -2,70 +2,93 @@
  * Basic tree that stores a value.
  */
 
-var Tree = function(value) {
-    this.value = value;
-    this.children = [];
-  };
-  
-  
-  
-  Tree.prototype.BFSelect = function(filter) {
-    // return an array of values for which the function filter(value, depth) returns true
-  };
-  
-  /**
+const Tree = function (value) {
+  this.value = value;
+  this.children = [];
+};
+
+// return an array of values for which the function filter(value, depth) returns true
+Tree.prototype.BFSelect = function (filter) {
+  const result = [];
+  // create a queue
+  const queue = [];
+  // push the root to the queue
+  queue.unshift(this);
+  let depth = 0;
+  // while queue is not empty
+  while (queue.length) {
+    // set the current size of the queue to be level size
+    let levelSize = queue.length;
+    // while level size is not zero
+    while (levelSize) {
+      // dequeue
+      const node = queue.pop();
+      levelSize -= 1;
+      // if filter is true, push to the result
+      if (filter(node.value, depth)) {
+        result.push(node.value);
+      }
+      // push the children into the queue
+      node.children.forEach((child) => {
+        queue.unshift(child);
+      });
+    }
+    depth += 1;
+  }
+  return result;
+};
+
+/**
    * You shouldn't need to change anything below here, but feel free to look.
     */
-  
-  /**
-    * add an immediate child
-    * (wrap values in Tree nodes if they're not already)
-    */
-  Tree.prototype.addChild = function(child) {
-    if (!child || !(child instanceof Tree)) {
-      child = new Tree(child);
-    }
-  
-    if (!this.isDescendant(child)) {
-      this.children.push(child);
-    } else {
-      throw new Error('That child is already a child of this tree');
-    }
-    // return the new child node for convenience
-    return child;
-  };
-  
-  /**
+
+/**
     * check to see if the provided tree is already a child of this
     * tree __or any of its sub trees__
     */
-  Tree.prototype.isDescendant = function(child) {
-    if (this.children.indexOf(child) !== -1) {
-      // `child` is an immediate child of this tree
+Tree.prototype.isDescendant = function (child) {
+  if (this.children.indexOf(child) !== -1) {
+    // `child` is an immediate child of this tree
+    return true;
+  }
+  for (let i = 0; i < this.children.length; i += 1) {
+    if (this.children[i].isDescendant(child)) {
+      // `child` is descendant of this tree
       return true;
-    } else {
-      for (var i = 0; i < this.children.length; i++) {
-        if (this.children[i].isDescendant(child)) {
-          // `child` is descendant of this tree
-          return true;
-        }
-      }
-      return false;
     }
-  };
-  
-  /**
-    * remove an immediate child
+  }
+  return false;
+};
+
+/**
+        * add an immediate child
+        * (wrap values in Tree nodes if they're not already)
+        */
+Tree.prototype.addChild = function (child) {
+  if (!child || !(child instanceof Tree)) {
+    child = new Tree(child);
+  }
+
+  if (!this.isDescendant(child)) {
+    this.children.push(child);
+  } else {
+    throw new Error('That child is already a child of this tree');
+  }
+  // return the new child node for convenience
+  return child;
+};
+
+/**
+     * remove an immediate child
     */
-  Tree.prototype.removeChild = function(child) {
-    var index = this.children.indexOf(child);
-    if (index !== -1) {
-      // remove the child
-      this.children.splice(index, 1);
-    } else {
-      throw new Error('That node is not an immediate child of this tree');
-    }
-  };
-  
+Tree.prototype.removeChild = function (child) {
+  const index = this.children.indexOf(child);
+  if (index !== -1) {
+    // remove the child
+    this.children.splice(index, 1);
+  } else {
+    throw new Error('That node is not an immediate child of this tree');
+  }
+};
 
 module.exports = Tree;
--- index.test.js
+++ index.test.js
@@ -1,7 +1,26 @@
 const Tree = require('./index');
 
 describe('Test the implementation of BFSelect', () => {
-  it('should ...', () => {
-    expect().toBe();
+  it('should filter by value', () => {
+    const root1 = new Tree(1);
+    const branch2 = root1.addChild(2);
+    const branch3 = root1.addChild(3);
+    const leaf4 = branch2.addChild(4);
+    const leaf5 = branch2.addChild(5);
+    const leaf6 = branch3.addChild(6);
+    const leaf7 = branch3.addChild(7);
+    const filter = (value, depth) => value % 2;
+    expect(root1.BFSelect(filter)).toEqual([1, 3, 5, 7]);
+  });
+  it('should filter by depth', () => {
+    const root1 = new Tree(1);
+    const branch2 = root1.addChild(2);
+    const branch3 = root1.addChild(3);
+    const leaf4 = branch2.addChild(4);
+    const leaf5 = branch2.addChild(5);
+    const leaf6 = branch3.addChild(6);
+    const leaf7 = branch3.addChild(7);
+    const filter = (value, depth) => depth === 1;
+    expect(root1.BFSelect(filter)).toEqual([2, 3]);
   });
 });
